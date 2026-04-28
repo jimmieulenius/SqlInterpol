367abfa3fcda7a832017cae63ceaed446122cdec
Jimmie Ulenius <jimmie.ulenius@outlook.com>
2026-04-28 10:38:26 +0200

    refactor: Remove commented-out SQL column attributes and unnecessary whitespace in Product model

--- src/SqlInterpol.Test/Models/Product.cs
+++ src/SqlInterpol.Test/Models/Product.cs
@@ -1,5 +1,3 @@
-
-
 using SqlInterpol.Metadata;
 
 namespace SqlInterpol.Test.Models;
@@ -7,15 +5,9 @@ namespace SqlInterpol.Test.Models;
 [SqlTable(schema: "dbo")]
 public class Product
 {
-    /// <summary>Product item number (SKU)</summary>
-    // [SqlColumn]
     public int ItemNumber { get; set; }
 
-    /// <summary>Product name - mapped from database column 'ProductName'</summary>
-    // [SqlColumn("ProductName")]
     public string Name { get; set; } = null!;
 
-    /// <summary>Product price</summary>
-    // [SqlColumn]
     public decimal Price { get; set; }
 }
