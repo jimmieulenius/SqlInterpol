d890ae8e18a6b50617b2f80af343da08c29c3bf2
Jimmie Ulenius <jimmie.ulenius@outlook.com>
2026-05-17 19:33:22 +0200

    feat: Add Window Function tests for multiple SQL dialects with Wysiwyg assertions

---
+++ src/SqlInterpol.Test/WindowFunctionTests.cs
@@ -0,0 +1,118 @@
+using SqlInterpol.Config;
+using SqlInterpol.Test.Dialects;
+using SqlInterpol.Test.Models;
+
+namespace SqlInterpol.Test;
+
+public class WindowFunctionTests
+{
+    [Theory]
+    [MemberData(nameof(WindowFunctionData))]
+    public void Select_WindowFunction_Wysiwyg_WorksAcrossDialects(SqlTestCase testCase)
+    {
+        // Arrange
+        var db = testCase.CreateBuilder();
+
+        // Act
+        var result = db.Query<Product>(p => db.Append($$"""
+            SELECT
+                {{p[x => x.Name]}},
+                SUM({{p[x => x.Price]}}) OVER (
+                    PARTITION BY {{p[x => x.CategoryId]}}
+                    ORDER BY {{p[x => x.Id]}} DESC
+                ) AS CategoryTotal
+            FROM {{p}}
+            """)).Build();
+
+        // Assert
+        testCase.AssertSql(result.Sql);
+    }
+
+    public static TheoryData<SqlTestCase> WindowFunctionData =>
+    [
+        new SqlTestCase(
+            SqlDialectKind.CustomDb,
+            [
+                """
+                SELECT
+                    <<dbo>>.<<Products>>.<<PROD_NAME>>,
+                    SUM(<<dbo>>.<<Products>>.<<Price>>) OVER (
+                        PARTITION BY <<dbo>>.<<Products>>.<<CategoryId>>
+                        ORDER BY <<dbo>>.<<Products>>.<<Id>> DESC
+                    ) AS CategoryTotal
+                FROM <<dbo>>.<<Products>>
+                """
+            ]
+        ),
+        new SqlTestCase(
+            SqlDialectKind.MySql,
+            [
+                """
+                SELECT
+                    `dbo`.`Products`.`PROD_NAME`,
+                    SUM(`dbo`.`Products`.`Price`) OVER (
+                        PARTITION BY `dbo`.`Products`.`CategoryId`
+                        ORDER BY `dbo`.`Products`.`Id` DESC
+                    ) AS CategoryTotal
+                FROM `dbo`.`Products`
+                """
+            ]
+        ),
+        new SqlTestCase(
+            SqlDialectKind.Oracle,
+            [
+                """
+                SELECT
+                    "dbo"."Products"."PROD_NAME",
+                    SUM("dbo"."Products"."Price") OVER (
+                        PARTITION BY "dbo"."Products"."CategoryId"
+                        ORDER BY "dbo"."Products"."Id" DESC
+                    ) AS CategoryTotal
+                FROM "dbo"."Products"
+                """
+            ]
+        ),
+        new SqlTestCase(
+            SqlDialectKind.PostgreSql,
+            [
+                """
+                SELECT
+                    "dbo"."Products"."PROD_NAME",
+                    SUM("dbo"."Products"."Price") OVER (
+                        PARTITION BY "dbo"."Products"."CategoryId"
+                        ORDER BY "dbo"."Products"."Id" DESC
+                    ) AS CategoryTotal
+                FROM "dbo"."Products"
+                """
+            ]
+        ),
+        new SqlTestCase(
+            SqlDialectKind.SqLite,
+            [
+                """
+                SELECT
+                    "dbo"."Products"."PROD_NAME",
+                    SUM("dbo"."Products"."Price") OVER (
+                        PARTITION BY "dbo"."Products"."CategoryId"
+                        ORDER BY "dbo"."Products"."Id" DESC
+                    ) AS CategoryTotal
+                FROM "dbo"."Products"
+                """
+            ]
+        ),
+        new SqlTestCase(
+            SqlDialectKind.SqlServer,
+            [
+                """
+                SELECT
+                    [dbo].[Products].[PROD_NAME],
+                    SUM([dbo].[Products].[Price]) OVER (
+                        PARTITION BY [dbo].[Products].[CategoryId]
+                        ORDER BY [dbo].[Products].[Id] DESC
+                    ) AS CategoryTotal
+                FROM [dbo].[Products]
+                """
+            ]
+        )
+    ];
+}
